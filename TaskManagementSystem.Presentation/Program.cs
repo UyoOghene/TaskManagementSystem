04b720c7a36bd2b6e79a2ddf122406fa6729908e
Akpujiha Uyo-Oghene <uyooghene01@gmail.com>
2025-08-12 16:37:38 +0100

    first commit

---
+++ TaskManagementSystem.Presentation/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
+using TaskManagementSystem.Application;
+using TaskManagementSystem.Infrastructure;
+using TaskManagementSystem.Presentation;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddApplication();
+        services.AddInfrastructure();
+        services.AddFeatureManagement(configuration.GetSection("FeatureManagement"));
+        services.AddSingleton<App>();
+    })
+    .Build();
+
+var app = host.Services.GetRequiredService<App>();
+await app.RunAsync();
